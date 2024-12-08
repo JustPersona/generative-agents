afbb7748c8ab6d2d9f3a53e1d0a58ab278b6e7aa
jh1950 <rlawnsgl191@gmail.com>
2024-12-09 00:44:50 +0900

    updates
    cors, compose, readme

--- environment/frontend_server/config/settings.py
+++ environment/frontend_server/config/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "account",
     "translator",
     "sass_processor",
+    "corsheaders",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
